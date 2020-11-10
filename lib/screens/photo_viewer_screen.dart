15cc943475a22982555c22c846fc40264c3d076c
Marcus Ng <marcuslistanng@gmail.com>
2020-11-09 21:29:58 -0500

    add basic navigation

---
+++ lib/screens/photo_viewer_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PhotoViewerScreen extends StatefulWidget {
+  @override
+  _PhotoViewerScreenState createState() => _PhotoViewerScreenState();
+}
+
+class _PhotoViewerScreenState extends State<PhotoViewerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/widgets/photo_card.dart
+++ lib/widgets/photo_card.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_photos/models/models.dart';
+import 'package:flutter_photos/screens/screens.dart';
 
 class PhotoCard extends StatelessWidget {
   final Photo photo;
@@ -13,20 +14,27 @@ class PhotoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[50],
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            offset: Offset(0, 2),
-            blurRadius: 4.0,
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => PhotoViewerScreen(),
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[50],
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 4.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: CachedNetworkImageProvider(photo.url),
+            fit: BoxFit.cover,
           ),
-        ],
-        image: DecorationImage(
-          image: CachedNetworkImageProvider(photo.url),
-          fit: BoxFit.cover,
         ),
       ),
     );
